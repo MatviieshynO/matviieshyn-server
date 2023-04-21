31ef599892f1d4e52c14ec44a64b43f391dc6675
MatviieshynO <Olopromotion@gmail.com>
2023-04-22 00:30:18 +0200

    deploy confiq

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ import Message from './Message.js'
 
 dotenv.config()
 const server = express()
-server.use(cors())
 //dotenv
 const { PORT, DB_NAME, DB_PASSWORD, DB_USER } = process.env
 //midleware
+server.use(cors())
 server.use(express.json())
 //routes
 server.post('/message', async (req, res) => {
@@ -31,7 +31,7 @@ server.post('/message', async (req, res) => {
     })
   } catch (error) {
     res.json({
-      message: `Error sending message`,
+      message: `Error sending message `,
     })
   }
 })
